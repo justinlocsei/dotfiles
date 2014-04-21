7041e889f40b752e2459a71cc1a41de8adf37b99
Justin Locsei <justin.locsei@gmail.com>
2014-04-21 09:35:06 -0400

    Normalize indentation

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -19,23 +19,23 @@ style_branch="${SOLAR_CYAN}"
 
 # Git status
 function prompt_git() {
- local status output flags
- status="$(git status 2>/dev/null)"
- [[ $? != 0 ]] && return;
- output="$(echo "$status" | awk '/# Initial commit/ {print "(init)"}')"
- [[ "$output" ]] || output="$(echo "$status" | awk '/# On branch/ {print $4}')"
- [[ "$output" ]] || output="$(git branch | perl -ne '/^\* (.*)/ && print $1')"
- flags="$(
- echo "$status" | awk 'BEGIN {r=""} \
-     /^# Changes to be committed:$/        {r=r "+"}\
-     /^# Changes not staged for commit:$/  {r=r "!"}\
-     /^# Untracked files:$/                {r=r "?"}\
-     END {print r}'
- )"
- if [[ "$flags" ]]; then
-     output="$output[$flags]"
- fi
- echo -ne "${SOLAR_WHITE} on ${style_branch}${output}"
+  local status output flags
+  status="$(git status 2>/dev/null)"
+  [[ $? != 0 ]] && return;
+  output="$(echo "$status" | awk '/# Initial commit/ {print "(init)"}')"
+  [[ "$output" ]] || output="$(echo "$status" | awk '/# On branch/ {print $4}')"
+  [[ "$output" ]] || output="$(git branch | perl -ne '/^\* (.*)/ && print $1')"
+  flags="$(
+  echo "$status" | awk 'BEGIN {r=""} \
+    /^# Changes to be committed:$/        {r=r "+"}\
+    /^# Changes not staged for commit:$/  {r=r "!"}\
+    /^# Untracked files:$/                {r=r "?"}\
+    END {print r}'
+  )"
+  if [[ "$flags" ]]; then
+    output="$output[$flags]"
+  fi
+  echo -ne "${SOLAR_WHITE} on ${style_branch}${output}"
 }
 
 # Build the prompt
