549ce52bbeabd0e4cf1d358b078191e309e08b12
Justin Locsei <justin.locsei@gmail.com>
2015-12-09 19:58:20 -0500

    Normalize quotes in the bash aliases

--- bash/scripts/aliases.sh
+++ bash/scripts/aliases.sh
@@ -1,37 +1,37 @@
 # Use an OS-specific rich-list function
 if [[ `uname -s` == "Darwin" ]]; then
-  alias ll='ls -Glh'
+  alias ll="ls -Glh"
 else
-  alias ll='ls -lh --color=auto'
+  alias ll="ls -lh --color=auto"
 fi
 
 # Profile management
-alias sbp='source ~/.bash_profile'
-alias vbp='vi ~/.bash_profile'
+alias sbp="source ~/.bash_profile"
+alias vbp="vi ~/.bash_profile"
 
 # Ruby
-alias bcon='bundle exec rails console'
-alias bcuke='bundle exec cucumber'
-alias be='bundle exec'
-alias brails='bundle exec rails'
-alias brake='bundle exec rake'
-alias bspec='bundle exec rspec'
-alias zcon='zeus console'
-alias zcuke='zeus cucumber'
-alias zrake='zeus rake'
-alias zs='zeus server'
-alias zspec='zeus rspec'
-alias zu='zeus start'
+alias bcon="bundle exec rails console"
+alias bcuke="bundle exec cucumber"
+alias be="bundle exec"
+alias brails="bundle exec rails"
+alias brake="bundle exec rake"
+alias bspec="bundle exec rspec"
+alias zcon="zeus console"
+alias zcuke="zeus cucumber"
+alias zrake="zeus rake"
+alias zs="zeus server"
+alias zspec="zeus rspec"
+alias zu="zeus start"
 
 # Git
-alias gba='git branch -a'
-alias gco='git checkout'
-alias gd='git diff'
-alias gl='git log'
-alias gs='git status'
+alias gba="git branch -a"
+alias gco="git checkout"
+alias gd="git diff"
+alias gl="git log"
+alias gs="git status"
 
 # Node.js
-alias npe='PATH=$(npm bin):$PATH'
+alias npe="PATH=$(npm bin):$PATH"
 
 # Show all processes that match a name
 pag() {
