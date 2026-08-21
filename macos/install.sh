c0619fe02409a67ff38038be9acffbc469f1ee50
Justin Locsei <justin.locsei@gmail.com>
2026-08-21 08:52:34 -0400

    Update wording

--- macos/install.sh
+++ macos/install.sh
@@ -3,6 +3,6 @@
 if [[ "$(defaults read -g ApplePressAndHoldEnabled)" == "0" ]]; then
   skip_action "Press and hold is already disabled"
 else
-  perform_action "Disabling press and hold"
+  perform_action "Disable press and hold"
   defaults write -g ApplePressAndHoldEnabled -bool false
 fi
