d8d3234f43ce504e8f946efed69764c63f8440b2
Justin Locsei <justin.locsei@gmail.com>
2026-08-21 14:23:13 -0400

    Find hidden files

--- shell/install.sh
+++ shell/install.sh
@@ -1,14 +1,10 @@
 # shellcheck shell=bash
 
-install_files() {
-  for file in "$(child_path shell/config)"/*; do
-    local name
-    name="$(basename "$file")"
+(
+  shopt -s dotglob nullglob
 
+  for file in "$(child_path shell/config)"/*; do
+    name="$(basename "$file")"
     ensure_symlink_exists "$name" "$file" "$HOME/$name"
   done
-}
-
-install_files
-
-unset -f install_files
+)
