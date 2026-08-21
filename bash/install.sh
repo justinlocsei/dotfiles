6620b10cff0ab693df2bc7872185105f05a7c446
Justin Locsei <justin.locsei@gmail.com>
2026-08-21 14:23:13 -0400

    Simplify shared config

--- bash/install.sh
+++ bash/install.sh
@@ -1,15 +1,3 @@
 # shellcheck shell=bash
 
-LOADER="$(child_path shell/scripts/source.sh)"
-
-PROFILE="$HOME/.bash_profile"
-touch "$PROFILE"
-
-SOURCE_LINE="source $LOADER"
-
-if ! grep -Fqx "$SOURCE_LINE" "$PROFILE"; then
-  perform_action "Sourcing shell loader in bash profile"
-  echo "$SOURCE_LINE" >> "$PROFILE"
-else
-  skip_action "Already sourcing shell loader in bash profile"
-fi
+install_script_loader "$HOME/.bash_profile"
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,15 +1,3 @@
 # shellcheck shell=bash
 
-LOADER="$(child_path shell/scripts/source.sh)"
-
-PROFILE="$HOME/.zshrc"
-touch "$PROFILE"
-
-SOURCE_LINE="source $LOADER"
-
-if ! grep -Fqx "$SOURCE_LINE" "$PROFILE"; then
-  perform_action "Sourcing shell loader in zsh profile"
-  echo "$SOURCE_LINE" >> "$PROFILE"
-else
-  skip_action "Already sourcing shell loader in zsh profile"
-fi
+install_script_loader "$HOME/.zshrc"
